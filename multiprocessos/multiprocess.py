ddb95c589915aeb79f6b62948faf7aa261ee04c1
Hythan <hythan97@gmail.com>
2019-08-23 15:15:08 -0300

    atv 1 multiprocessos

---
+++ multiprocessos/multiprocess.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy
+import random
+import time
+import logging
+import multiprocessing
+
+def cria_matriz(linhas, colunas):
+  A = []
+  for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+      linha = linha + [random.randint(1, 10)]
+    A = A + [linha]
+  return A
+
+def multiplica_linha_coluna(queue, queue_resultados, matrizA, matrizB):
+    while True:
+        i, j = queue.get()
+        valor = 0
+        for k in range(len(matrizB)):
+            valor = valor + matrizA[i][k] * matrizB[k][j]
+
+        queue.task_done()
+        queue_resultados.put((i, j, valor))
+
+if __name__ == '__main__':
+    linhas, colunas = 400, 400
+
+    print("{}: Gerando matrizes".format(time.strftime('%c')))
+    matrizA = cria_matriz(linhas, colunas)
+    matrizB = cria_matriz(linhas, colunas)
+    matrizC = numpy.zeros(shape=(linhas,colunas))
+
+    print("{}: Multiplicando matrizes".format(time.strftime('%c')))
+    queue = multiprocessing.JoinableQueue()
+    queue_resultados = multiprocessing.JoinableQueue()
+
+    for i in range(3):
+        worker = multiprocessing.Process(target=multiplica_linha_coluna, args=(queue,queue_resultados,matrizA,matrizB,))
+        worker.daemon = True
+        worker.start()
+    
+
+    for i in range(len(matrizA)):
+        for j in range(len(matrizA[0])):
+            queue.put((i, j))
+        
+    queue.join()
+
+    while not queue_resultados.empty():
+        i, j, valor = queue_resultados.get()
+        matrizC[i][j] = valor
+        queue_resultados.task_done()
+
+    print("{}: Resultado:{}".format(time.strftime('%c'), matrizC))
